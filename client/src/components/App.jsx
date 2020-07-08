4e9badf49f94f462ea4773b95a11e8ed7fe5a09a
alex <axmitchell202@gmail.com>
2020-07-08 00:48:36 -0400

    Simplify google.map styles

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -71,25 +71,7 @@ class App extends React.Component {
             "elementType": "geometry",
             "stylers": [
               {
-                "color": "#000000"
-              },
-              {
-                "visibility": "on"
-              }
-            ]
-          },
-          {
-            "featureType": "administrative.country",
-            "elementType": "geometry.stroke",
-            "stylers": [
-              {
-                "color": "#000000"
-              },
-              {
-                "visibility": "on"
-              },
-              {
-                "weight": 1.5
+                "visibility": "off"
               }
             ]
           },
@@ -99,9 +81,6 @@ class App extends React.Component {
             "stylers": [
               {
                 "color": "#000000"
-              },
-              {
-                "visibility": "on"
               }
             ]
           },
@@ -122,86 +101,6 @@ class App extends React.Component {
             ]
           },
           {
-            "featureType": "road",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#2c2c2c"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "labels.icon",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#8a8a8a"
-              }
-            ]
-          },
-          {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#373737"
-              }
-            ]
-          },
-          {
-            "featureType": "road.highway",
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#3c3c3c"
-              }
-            ]
-          },
-          {
-            "featureType": "road.highway.controlled_access",
-            "elementType": "geometry",
-            "stylers": [
-              {
-                "color": "#4e4e4e"
-              }
-            ]
-          },
-          {
-            "featureType": "road.local",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#616161"
-              }
-            ]
-          },
-          {
-            "featureType": "transit",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "transit",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#757575"
-              }
-            ]
-          },
-          {
             "featureType": "water",
             "elementType": "geometry",
             "stylers": [
@@ -210,27 +109,6 @@ class App extends React.Component {
               }
             ]
           },
-          {
-            "featureType": "water",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#000000"
-              },
-              {
-                "visibility": "on"
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#3d3d3d"
-              }
-            ]
-          }
         ]
       });
       map.data.loadGeoJson(`http://localhost:3000/map`);
